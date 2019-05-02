ed8a0c3e7c0c707a1b3f96ee6f457cbfda936dba
Dawid Cyron <dawidcyron@op.pl>
2019-05-02 18:52:39 +0200

    Add fields and constructor to cryptocurrency  class

---
+++ lib/cryptocurrency/cryptocurrency.dart
@@ -0,0 +1,24 @@
+class Cryptocurrency {
+  String type;
+  String fromSymbol;
+  double price;
+  String lastUpdate;
+  String openDay;
+  String highDay;
+  String lowDay;
+  double changePtcDay;
+  String imageUrl;
+
+  Cryptocurrency({
+    this.type,
+    this.fromSymbol,
+    this.price,
+    this.lastUpdate,
+    this.openDay,
+    this.highDay,
+    this.lowDay,
+    this.changePtcDay,
+    this.imageUrl
+  });
+  
+}
